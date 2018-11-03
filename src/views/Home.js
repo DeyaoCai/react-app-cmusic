e8d2d8d7c838f52c5b4abc1c5b668c61dc039971
DeyaoCai <32273577+DeyaoCai@users.noreply.github.com>
2018-11-04 01:41:17 +0800

    no message

---
+++ src/views/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import '../styles/Index.css';
+
+class Index extends Component {
+  render() {
+    return (
+      <div className="App">
+          <img src={logo} className="App-logo" alt="logo" />
+      </div>
+    );
+  }
+}
+export default Index;
---
+++ src/views/My.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import '../styles/Home.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="Home"></div>
+    );
+  }
+}
+export default Home;
